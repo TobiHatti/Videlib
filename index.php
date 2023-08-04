73945e0bc3bb34af32816accb99e9bc315221505
Tobias Hattinger <tobias.hattinger@gmx.at>
2023-08-04 14:22:28 +0200

    Added opengraph and twitter meta tags

--- index.php
+++ index.php
@@ -7,18 +7,39 @@ require("lib/wrapsql.php");
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Videlib</title>
+	<meta name="description" content="Character lexicon and manager for fiction, fantasy or RP, with several ways to manage and organize characters"/>
+
+	<meta property="og:site_name" content="Vide" />
+	<meta property="og:description" content="Character lexicon and manager for fiction, fantasy or RP, with several ways to manage and organize characters" />
+	<meta property="og:image" content="/files/VideLogo.png">
+	<meta property="og:image:type" content="image/png">
+	<meta property="og:image:width" content="1024">
+	<meta property="og:image:height" content="1024">
+	<meta property="og:image:alt" content="Vide Logo" />
+	<meta property="og:title" content="Vide" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://vide.endev.at" />
+	<meta property="og:determiner" content="a" />
+	<meta property="og:locale" content="en_US" />
+	
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:title" content="Vide" />
+	<meta name="twitter:description" content="Character lexicon and manager for fiction, fantasy or RP, with several ways to manage and organize characters" />
+	<meta name="twitter:image" content="/files/VideLogo.png" />
+	<meta name="twitter:image:alt" content="Vide Logo" />
+
+	<title>Vide</title>
+	<link rel="icon" type="image/x-icon" href="/files/favicon.ico">
 	<link rel="stylesheet" href="/css/layout.css">
 	<link rel="stylesheet" href="/css/particles.css">
 	<link rel="stylesheet" href="/css/form.css">
 	<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-	
 	<script src="/js/script.js"></script>
 </head>
 <body>
 	<div id="particleContainer"></div>
 	<header>
-		<h1>Videlib</h1>
+		<h1>Vide</h1>
 	</header>
 	<main></main>
 	<script src="/js/events.js"></script>
